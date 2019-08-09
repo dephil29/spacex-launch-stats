4f29e025f2929acc06ede36a2c4c562d94dfdf8e
Eric Phillips <dephil29@hotmail.com>
2019-08-09 14:43:56 -0400

    finished front-end

---
+++ public/precache-manifest.82ba51adf7d8734e3c3984fc2638d787.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0a33259f462cd417557465170b8a74de",
+    "url": "/index.html"
+  },
+  {
+    "revision": "9ce3e1864b203e4ceaf7",
+    "url": "/static/css/main.cfda8877.chunk.css"
+  },
+  {
+    "revision": "dbf6b9577e9d784deee4",
+    "url": "/static/js/2.32b8a153.chunk.js"
+  },
+  {
+    "revision": "9ce3e1864b203e4ceaf7",
+    "url": "/static/js/main.0c78cea1.chunk.js"
+  },
+  {
+    "revision": "fdfcfda2d9b1bf31db52",
+    "url": "/static/js/runtime~main.c5541365.js"
+  },
+  {
+    "revision": "ab10d000cdf7ee45a57395feee160ad2",
+    "url": "/static/media/logo.ab10d000.png"
+  }
+]);
